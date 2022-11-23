7242cc1c157c4f205f73b0d7a36fb0c54b3f4324
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-11-22 23:31:51 -0600

    twosum
    Leetcode easy problems are still pretty hard.  I am most likely missing something here.

--- 1_twosum.py
+++ 1_twosum.py
@@ -6,9 +6,94 @@ https://leetcode.com/problems/two-sum/?envType=study-plan&id=data-structure-i
 class Solution:
     # If you can find it in one pass, great, otherwise accumulate lower values to separate list
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        newnums = sorted(enumerate(nums))
-        i, j = 0, len(newnums)-1
-        while newnums
+        sort_nums = [x for x in enumerate(nums)]
+        sort_nums.sort(key=lambda x: x[1])
+        step = 0
+        low = 0
+        h = len(nums) - 1
+        m = (low + h) // 2
+        while step < len(nums) -1:
+            low = step
+            h = len(nums) - 1
+            m = (low + h) // 2
+            if target == 0 and nums.count(0) >= 2:
+                find = 0
+                while sort_nums[m][1] != find and h - low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
+            if target == 0:
+                find = sort_nums[step][1] * -1
+                while sort_nums[m][1] != find and h - low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
+            if 0 > target > sort_nums[step][1]:
+                find = sort_nums[step][1] - target
+                while sort_nums[m][1] != find and h-low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
+            elif 0 > target < sort_nums[step][1]:
+                find = sort_nums[step][1] - target
+                while sort_nums[m][1] != find and h-low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
+            elif 0 < target > sort_nums[step][1]:
+                find = target + sort_nums[step][1]
+                while sort_nums[m][1] != find and h-low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
+            elif 0 < target < sort_nums[step][1]:
+                find = target - sort_nums[step][1]
+                while sort_nums[m][1] != find and h-low > 1:
+                    if find < sort_nums[m][1]:
+                        h = m
+                        m = (low + h) // 2
+                    elif find > sort_nums[m][1]:
+                        low = m
+                        m = (low + h) // 2
+                if sort_nums[m][1] == find:
+                    return [sort_nums[step][0], sort_nums[m][0]]
+                else:
+                    step += 1
 
 if __name__ == "__main__":
     S = Solution()
@@ -16,4 +101,4 @@ if __name__ == "__main__":
     testcase2 = ([5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5], 10)
     testcase3 = ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1)
     testcase4 = ([0, 4, 3, 0], 0)
-    print(S.twoSum(*testcase4))
+    print(S.twoSum(*testcase))
--- 217_containsduplicate.py
+++ 217_containsduplicate.py
@@ -1,46 +1,21 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        if len(nums) < 12:
-            l = []
-            for x in nums:
-                if x in l:
-                    return True
-                else:
-                    l.append(x)
-            return False
-        else:
+        i = 0
+        j = 1
+        while j < len(nums):
+            nu
 
-            l = sorted(nums)
-            a, b, c, d, e, f = len(l) - 1, len(l) - 2, len(l) - 3, len(l) - 4, len(l) - 5, len(l) - 6
-            t, u, v, x, y, z = 0, 1, 2, 3, 4, 5
-
-            while t <= (len(l) // 2) + 6:
-                print(a, d, c, d, e, f)
-                if l[a] == l[b] or l[b] == l[c] or l[c] == l[d] or l[d] == l[e] or l[e] == l[f]:
-                    return True
-                else:
-                    a -= 6
-                    b -= 6
-                    c -= 6
-                    d -= 6
-                    e -= 6
-                    f -= 6
-
-                if t ==l[u] or l[u] == l[v] or l[v] == l[x] or l[x] == l[y] or l[y] == l[z]:
-                    return True
-                else:
-                    t += 6
-                    u += 6
-                    v += 6
-                    x += 6
-                    y += 6
-                    z += 6
-
-            return False
 
 if __name__ == "__main__":
     test = [1, 2, 3, 1]
     test2 = [1, 2, 3, 4]
-    test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, ]
+    test3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, ]
+    test4 = [959, -123, 811, -940, 663, 560, 293, -180, 871, 794, 927, 135, 125, -583, 474, -834, -898, 912, 500, -158,
+             -76, 260, 542, -716,
+             320, 781, -413, -5, -899, 868, 899, 127, -739, -650, -32, -150, -296, -422, -390, -476, 514, 513, 337,
+             -467, -168, 344, -511,
+             -383, -10, 142, 370, -192, -754, -657, 318, -668, -846, -574, 26, 856, 663, 19, 128, -549, -72, 857, -318,
+             555, -78, -124, -714, 122,
+             -605, -500, -814, -642, -299, 139]
     s = Solution()
-    print(s.containsDuplicate(test3))
+    print(s.containsDuplicate(test4))
