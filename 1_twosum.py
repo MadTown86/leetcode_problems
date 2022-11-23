89505d42cdb70496575641067d093b0dec7feecb
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-11-22 23:46:20 -0600

    twosum
    Leetcode easy problems are still pretty hard.  I am most likely missing something here.

--- 1_twosum.py
+++ 1_twosum.py
@@ -10,14 +10,20 @@ class Solution:
         sort_nums.sort(key=lambda x: x[1])
         step = 0
         low = 0
+        find = None
+        lastfind = 0
         h = len(nums) - 1
         m = (low + h) // 2
-        while step < len(nums) -1:
+        while step < len(nums):
             low = step
             h = len(nums) - 1
             m = (low + h) // 2
             if target == 0 and nums.count(0) >= 2:
+                lastfind = find
                 find = 0
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h - low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -30,7 +36,11 @@ class Solution:
                 else:
                     step += 1
             if target == 0:
+                lastfind = find
                 find = sort_nums[step][1] * -1
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h - low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -43,7 +53,11 @@ class Solution:
                 else:
                     step += 1
             if 0 > target > sort_nums[step][1]:
+                lastfind = find
                 find = sort_nums[step][1] - target
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h-low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -56,7 +70,11 @@ class Solution:
                 else:
                     step += 1
             elif 0 > target < sort_nums[step][1]:
+                lastfind = find
                 find = sort_nums[step][1] - target
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h-low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -69,7 +87,11 @@ class Solution:
                 else:
                     step += 1
             elif 0 < target > sort_nums[step][1]:
-                find = target + sort_nums[step][1]
+                lastfind = find
+                find = target - sort_nums[step][1]
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h-low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -82,7 +104,11 @@ class Solution:
                 else:
                     step += 1
             elif 0 < target < sort_nums[step][1]:
+                lastfind = find
                 find = target - sort_nums[step][1]
+                if find == lastfind:
+                    step += 1
+                    continue
                 while sort_nums[m][1] != find and h-low > 1:
                     if find < sort_nums[m][1]:
                         h = m
@@ -101,4 +127,4 @@ if __name__ == "__main__":
     testcase2 = ([5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5], 10)
     testcase3 = ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 1)
     testcase4 = ([0, 4, 3, 0], 0)
-    print(S.twoSum(*testcase))
+    print(S.twoSum(*testcase2))
