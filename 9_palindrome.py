9376f36864d306b628767ee5ef5124a25a44cc3f
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-01-18 14:53:34 -0600

    Palindrome easy leetcode.  Still on the lowest scale for memory usage and speed. (not to mention quantity of code)

---
+++ 9_palindrome.py
@@ -0,0 +1,38 @@
+from time import perf_counter
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        a = 0
+        x = str(x)
+        b = len(x)-1
+        m = (len(x)-1)/2
+        if len(x) == 2:
+            if x[0] != x[1]:
+                return False
+            else:
+                return True
+        elif len(x) % 2 == 0:
+            while x[a] == x[b] and b >= 0 and a < len(x)-1:
+                a = a + 1
+                b = b - 1
+            if x[a] != x[b]:
+                return False
+            else:
+                return True
+        elif len(x) % 2 != 0:
+            while x[a] == x[b] and b != m and a != m:
+                a = a + 1
+                b = b - 1
+            if x[a] != x[b]:
+                return False
+            else:
+                return True
+
+if __name__ == "__main__":
+    S = Solution()
+    start = perf_counter()
+    res = S.isPalindrome(12321232123212321232123212321232123212321232123212321232123212321232123212321232123212321)
+    stop = perf_counter()
+    timer = stop - start
+    print(res)
+    print(f'{timer:.8f}')
