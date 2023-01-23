ba339d703e028d784c68e325d765a292b288927b
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-01-23 16:26:21 -0600

    Work update, refactoring problem.

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,9 +1,20 @@
+from typing import List
 class Solution:
-    def searchInsert(self, nums: list[int], target: int) -> int:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
 
         def recurse_inner(target, left, right):
-            if right - left < 3:
-                pos = left if nums[left] >= target else right
-                return pos
-            else:
-                pos = nums[left:right]
+            print(nums[left:right+1])
+            print(f'LEFT: {left}')
+            print(f'RIGHT {right}')
+            r_len = right - left
+
+
+        return recurse_inner(target, left, right)
+
+if __name__ == "__main__":
+    S = Solution()
+    args = [([1, 3, 5, 6], 5), ([1, 3, 5, 6], 2), ([1, 3, 5, 6], 7)]
+    for tups in args:
+        print(S.searchInsert(*tups))
