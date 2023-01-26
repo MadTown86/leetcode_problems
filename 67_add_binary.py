b8de40fe9dd71586307e32d74c968ce980c6ee57
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-01-25 22:43:35 -0600

    67_add_binary.py.  One more down.

--- 67_add_binary.py
+++ 67_add_binary.py
@@ -1,2 +1,25 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        av, bv = 0, 0
+        a = ''.join(x for x in reversed(a))
+        b = ''.join(x for x in reversed(b))
+        if len(a) > 1:
+            for x in range(0, len(a)):
+                if int(a[x]) == 1:
+                    av += 2**x
+        elif int(a[0]) == 1:
+            av = 1
+        if len(b) > 1:
+            for x in range(0, len(b)):
+                if int(b[x]) == 1:
+                    bv += 2**x
+        elif int(b[0]) == 1:
+            bv = 1
+        c = str(bin(av+bv))
+        return c[2:]
+
+if __name__ == "__main__":
+    S = Solution()
+    args = [['11', '1'], ['1010', '1011']]
+    for arg in args:
+        print(S.addBinary(*arg))
