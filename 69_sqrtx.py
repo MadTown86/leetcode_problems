ce8e8da672220c33049e619b2f5829e2135bcb1b
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-01-27 21:23:07 -0600

    Square root newton raphson method.

--- 69_sqrtx.py
+++ 69_sqrtx.py
@@ -1,15 +1,27 @@
+from math import floor
 class Solution:
+    # Had to look up a mathematical approach with powers/math functions not allowed
+    # newton raphson method sqrt
+    # At least learned a mathematical appraoch to obtaining the square root.
     def mySqrt(self, x: int) -> int:
-        b = 1
-        count = 1
-        while x:
-            x = x - b
-            b += 2
+        n = x
+        t = .00001
+        if x == 0:
+            return 0
+        while True:
+            root = .5 * (n + (x / n))
+            if (abs(root-n) < t):
+                break
+            n = root
 
-        return count
+        return floor(root)
+
+
+
+
 
 if __name__ == "__main__":
-    args = [4, 8]
+    args = [1, 0, 4, 8, 3, 13, 5, 75, 112, 1212]
     S = Solution()
     for arg in args:
         print(S.mySqrt(arg))
