38b8c2df133ef4f96afb7b836c026bade07100dd
MadTown86 <grover.donlon@gmail.com>
2023-02-14 13:44:36 -0600

    Work update.  Completed 88_merge_sorted_array in 30 min or so.

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -7,24 +7,32 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for x in range(m + n):
-            print(x)
-            print(nums1)
-            zero_ind = m + x + 1
-            nums2_val = nums2.pop(0)
-            if nums1[x] < nums2_val:
-                for y in range(zero_ind, x, -1):
-                     nums1[y] = nums1[y-1]
-                nums1[x + 1] = nums2_val
-
+        m_org = m
+        while nums2:
+            merge_val = nums2.pop(0)
+            for x in range(0, m+1):
+                if m == m_org + n:
+                    nums1[m_org + n] = merge_val
+                    break
+                if nums1[x] < merge_val and x != m:
+                    continue
+                if nums1[x] < merge_val and x == m:
+                    nums1[m] = merge_val
+                    m += 1
+                else:
+                    for y in range(len(nums1)-1, x-1, -1):
+                        nums1[y] = nums1[y-1]
+                    nums1[x] = merge_val
+                    m += 1
+                    break
 
 
 
 if __name__ == "__main__":
-    nums1 = [1, 2, 4, 6, 0, 0, 0, 0]
-    nums2 = [3, 5, 6, 8]
-    m = 4
-    n = 4
+    nums1 = [1, 2, 4, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    nums2 = [2, 3, 3, 5, 7, 8, 11, 13, 15, 17]
+    m = 5
+    n = 9
     S = Solution()
     S.merge(nums1, m, nums2, n)
     print(nums1)
