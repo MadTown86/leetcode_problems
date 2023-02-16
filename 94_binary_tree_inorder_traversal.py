3897835b2eaf8587a63de33c68829ec8f48d7b52
MadTown86 <grover.donlon@gmail.com>
2023-02-16 10:54:14 -0600

    Work update.  Completed 88_merge_sorted_array in 30 min or so.

--- 94_binary_tree_inorder_traversal.py
+++ 94_binary_tree_inorder_traversal.py
@@ -4,22 +4,16 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
+    r = []
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        r = []
-        if root.left:
-            r.append()
+        self._subtree(root)
+        return self.r
 
-
-
-if __name__ == '__main__':
-    T7 = TreeNode(val = 7)
-    T6 = TreeNode(val = 6)
-    T5 = TreeNode(val = 5, left=T6, right=T7)
-    T4 = TreeNode(val = 4)
-    T3 = TreeNode(val = 3)
-    T2 = TreeNode(val = 2, left=T3, right=T4)
-    T1 = TreeNode(val = 1, left=T2, right=T5)
-
-    S = Solution()
-    S.inorderTraversal(T1)
+    def _subtree(self, root: TreeNode):
+        if root.left != None:
+            self._subtree(root.left)
+        self.r.append(root.val)
+        if root.right != None:
+            self._subtree(root.right)
