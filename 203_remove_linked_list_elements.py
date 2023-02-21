413cc56fbbc19bdc0e6285f3384267c99a3230e9
MadTown86 <grover.donlon@gmail.com>
2023-02-21 15:33:48 -0600

    Solo Parenting leaves very little time for anything else...one problem in 4 days.

--- 203_remove_linked_list_elements.py
+++ 203_remove_linked_list_elements.py
@@ -1,26 +1,76 @@
+# https://leetcode.com/problems/remove-linked-list-elements/description/
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        if head.next is None:
+        if not head:
             return head
-        if head.
+        while head.val == val and head.next:
+            next_pos = head.next
+            head.next = None
+            head = next_pos
+        if head.val == val:
+            return None
+        if head and head.next and head.next.next:
+            last_pos = head
+            cursor = last_pos.next
+            next_pos = cursor.next
+            while next_pos:
+                if cursor.val == val:
+                    last_pos.next = next_pos
+                    cursor = next_pos
+                    next_pos = cursor.next
+                else:
+                    last_pos = cursor
+                    cursor = next_pos
+                    next_pos = cursor.next
+            if cursor.val == val:
+                last_pos.next = None
+            return head
+        else:
+            if head and not head.next:
+                if head.val != val:
+                    return head
+                else:
+                    return None
+            elif head.next.val == val:
+                head.next = None
+                return head
+            else:
+                return head
+
+
+
+
 
 if __name__ == "__main__":
-    T6 = ListNode(val=6)
-    T5 = ListNode(val=5, next=T6)
-    T4 = ListNode(val=5, next=T5)
-    T3 = ListNode(val=4, next=T4)
-    T2 = ListNode(val=3, next=T3)
-    T1 = ListNode(val=2, next=T2)
+    T7 = ListNode(val=6)
+    T6 = ListNode(val=5, next=T7)
+    T5 = ListNode(val=4, next=T6)
+    T4 = ListNode(val=3, next=T5)
+    T3 = ListNode(val=6, next=T4)
+    T2 = ListNode(val=2, next=T3)
+    T1 = ListNode(val=1, next=T2)
+
+    S4 = ListNode(val=7)
+    S3 = ListNode(val=7, next=S4)
+    S2 = ListNode(val=7, next=S3)
+    S1 = ListNode(val=7, next=S2)
+
+    SS1 = ListNode(val=8)
+
+    L1 = ListNode()
 
     S = Solution()
-    head = S.removeElements(T1, 5)
-
-    while head.next is not None:
-        print(head.val)
-        head = head.next
+    T = S.removeElements(T1, 6)
+    if not T:
+        print([])
+    else:
+        while T and T.next:
+            print(T.val)
+            T = T.next
+        print(T.val)
