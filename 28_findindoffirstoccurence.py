e20a856fc5628d059cc7483b6f71d2bdc80d54e7
MadTown86 <grover.donlon@gmail.com>
2023-03-17 14:44:41 -0500

    Work update.

--- 28_findindoffirstoccurence.py
+++ 28_findindoffirstoccurence.py
@@ -1,6 +1,22 @@
 """
 https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 """
+
+"""
+Attempting to practice the re module a bit here
+"""
+import re #so ronery
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        need = re.compile(needle)
+        match = re.search(need, haystack)
+        if match:
+            return match.start()
+        else:
+            return -1
+
+"""
+
 from functools import lru_cache
 import timer
 @lru_cache
@@ -53,3 +69,13 @@ if __name__ == "__main__":
     test1 = "iamnotada;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;woeijra;wlifna;slkdfj;alskdfja;lskdfj;woeifja;efioja;soiefja;soiefja;soefija;sliefja;lsiefj;asliefja;soiefj;asilefja;sliefja;sliefja;sliefja;sliefja;sliefja;sliefj;asliefj;asleifja;sleifja;soeijf;wlkjenf;lwjnerflkewajrntg;alkerjta;lskdjf;aslkdfj;aoweijf;aoeing;aoinse;foiasjef;lakesnmf;lawknwjef;aosid" + "jf;asoing;aowienf;asdkljfbugglycornstuglybuggly"
     test2 = "ibuggly"
     print(strStr4(test2, "buggly"))
+
+"""
+
+import re
+
+if __name__ == "__main__":
+    st = "bugglysmelly"
+    pattern = re.compile('ly')
+    match = re.search(pattern, st)
+    print(match.start())
