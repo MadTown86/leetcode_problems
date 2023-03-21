a57c1ff7cd2219fb712fa940b121fa49644a8fa3
MadTown86 <grover.donlon@gmail.com>
2023-03-21 16:04:28 -0500

    Work update.

--- 108_convert_sorted_to_binary_search_tree.py
+++ 108_convert_sorted_to_binary_search_tree.py
@@ -1,13 +1,25 @@
-# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
-from typing import List
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        if nums and len(nums) > 1:
-            prev = TreeNode(nums.pop())
-            cursor = TreeNode(nums.pop())
-        primrose = TreeNode(nums[0])
+    def isBalanced(self, root: TreeNode) -> bool:
+
+        if root is None:
+            return root
+
+        return max(self.isBalanced(root.left), self.isBalanced(root.right))
+
+    def height(self, root):
+        if root is None:
+            return True
+
+        lh = self.isBalanced(root.left)
+        rh = self.isBalanced(root.right)
+
+        if (abs(lh - rh) <= 1) and height(root.left) is True and height(root.right) is True:
+            return True
+
+        return False
+
