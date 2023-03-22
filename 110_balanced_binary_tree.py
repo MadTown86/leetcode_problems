c78d64875145e6680e33a9aedc264bb13bba39c2
MadTown86 <grover.donlon@gmail.com>
2023-03-21 22:48:08 -0500

    Start the next.

--- 110_balanced_binary_tree.py
+++ 110_balanced_binary_tree.py
@@ -5,35 +5,70 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+
+# function to find height of binary tree, had to find elsewhere. However, I should now have it fairly memorized
+
 class Solution:
-    def isBalanced(self, root: TreeNode) -> bool:
+    def height(self, root):
 
+        # base condition when binary tree is empty
         if root is None:
-            return root
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
 
-        return max(self.isBalanced(root.left), self.isBalanced(root.right))
+    # function to check if tree is height-balanced or not
 
-    def height(self, root) -> bool:
+
+    def isBalanced(self, root):
+
+        # Base condition
         if root is None:
             return True
 
-        lh = self.isBalanced(root.left)
-        rh = self.isBalanced(root.right)
+        # for left and right subtree height
+        lh = self.height(root.left)
+        rh = self.height(root.right)
 
-        if (abs(lh - rh) <= 1) and self.height(root.left) is True and self.height(root.right) is True:
+        # allowed values for (lh - rh) are 1, -1, 0
+        if (abs(lh - rh) <= 1) and self.isBalanced(
+                root.left) is True and self.isBalanced(root.right) is True:
             return True
 
+        # if we reach here means tree is not
+        # height-balanced tree
         return False
 
+
+
 if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.left.left.left = TreeNode(8)
+    S = Solution()
+    if S.isBalanced(root):
+        print("Tree is balanced")
+    else:
+        print("Tree is not balanced")
+
+
     N1 = TreeNode(3)
     N2 = TreeNode(9)
     N3 = TreeNode(20)
     N4 = TreeNode(15)
     N5 = TreeNode(7)
+    N6 = TreeNode(8)
+
+    N1.left = N2
+    N1.right = N3
+    N2.left = N4
+    N2.right = N5
+    N4.left = N6
 
 
-    L1 = TreeNode(1)
-    L2 = TreeNode(
-
-    )
+    S2 = Solution()
+    print(S.isBalanced(N1))
