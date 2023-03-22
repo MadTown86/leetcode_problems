f2d5c970b597ebf31c066662d76687e06c646ae6
MadTown86 <grover.donlon@gmail.com>
2023-03-22 13:50:04 -0500

    Work update.

--- 111_minimum_depth_binary_tree.py
+++ 111_minimum_depth_binary_tree.py
@@ -1,3 +1,6 @@
+from typing import Tuple, Any
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,8 +10,12 @@ class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-        if root.left:
-            return self.minDepth()
+        if root.left and root.right:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        elif root.left and not root.right:
+            return self.minDepth(root.left) + 1
+        else:
+            return self.minDepth(root.right) + 1
 
 
 if __name__ == '__main__':
@@ -23,5 +30,15 @@ if __name__ == '__main__':
     N3.left = N4
     N1.right = N5
 
+    L1 = TreeNode(2)
+    L3 = TreeNode(3)
+    L5 = TreeNode(4)
+    L6 = TreeNode(5)
+    L8 = TreeNode(6)
+    L1.right = L3
+    L3.right = L5
+    L5.right = L6
+    L6.right = L8
+
     S = Solution()
-    print(S.minDepth(N1))
+    print(S.minDepth(L1))
