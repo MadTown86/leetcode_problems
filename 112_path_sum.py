c422d116b24dd4b954c68f47991d59741eb8d988
MadTown86 <grover.donlon@gmail.com>
2023-03-24 14:58:43 -0500

    Work update.

--- 112_path_sum.py
+++ 112_path_sum.py
@@ -1,29 +1,52 @@
 #  https://leetcode.com/problems/path-sum/
+
+def cacheit(func):
+    counter = 0
+    def outter(func):
+        def inner(node, targetSum, valcarry=0):
+            nonlocal counter
+            counter += 1
+            print(f'VALCARRY: {valcarry}')
+            print(f'RECURSIVE LEVEL: {counter}')
+            print(f'NODE NAME: {node.name}')
+            print(f'NODE VALUE: {node.val}')
+            print(f'targetSum: {targetSum}')
+            return func(node, targetSum)
+        return outter
+    return inner
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, name: str = None, val=0, left=None, right=None):
+        self.name = name
         self.val = val
         self.left = left
         self.right = right
 class Solution:
+
+    @cacheit
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        if not root: #A
-            return targetSum == 0
+        if root.val:
+            print(root.val)
+        if not root:
+            return 0
         if root.left and root.right:
-            self.hasPathSum(root.left, targetSum - root.val)
-            self.hasPathSum(root.right, targetSum - root.val)
-        elif root.left
-        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val) #C1, C2
+            return self.hasPathSum(root.left, targetSum) + self.hasPathSum(root.right, targetSum) + root.val
+        elif root.left and not root.right:
+            return self.hasPathSum(root.left, targetSum) + root.val
+        elif root.right and not root.left:
+            return self.hasPathSum(root.right, targetSum) + root.val
+        else:
+            return root.val
 
 
 if __name__ == '__main__':
-    N1 = TreeNode(1)
-    N2 = TreeNode(2)
-    N3 = TreeNode(3)
-    N4 = TreeNode(4)
-    N5 = TreeNode(5)
-    N6 = TreeNode(6)
-    N7 = TreeNode(7)
-    N8 = TreeNode(8)
+    N1 = TreeNode(name="Node 1", val=1)
+    N2 = TreeNode(name="Node 2", val=2)
+    N3 = TreeNode(name="Node 3", val=3)
+    N4 = TreeNode(name="Node 4", val=4)
+    N5 = TreeNode(name="Node 5", val=5)
+    N6 = TreeNode(name="Node 6", val=6)
+    N7 = TreeNode(name="Node 7", val=7)
+    N8 = TreeNode(name="Node 8", val=8)
 
     N1.left = N2
     N1.right = N3
