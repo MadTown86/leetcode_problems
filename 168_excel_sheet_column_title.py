08cd76c5ff83bdcf9e148fd6539d0c2cad3590c0
MadTown86 <grover.donlon@gmail.com>
2023-03-31 14:48:35 -0500

    Work update.

---
+++ 168_excel_sheet_column_title.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/excel-sheet-column-title/
+
+import string
+
+caps = string.ascii_uppercase
+
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
