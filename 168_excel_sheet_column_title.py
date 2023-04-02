3f92b9351bece5a8ce43d641b6f903bfa723cc75
MadTown86 <grover.donlon@gmail.com>
2023-04-02 15:32:23 -0500

    Finally...

--- 168_excel_sheet_column_title.py
+++ 168_excel_sheet_column_title.py
@@ -6,4 +6,22 @@ caps = string.ascii_uppercase
 
 
 class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
+    def convertToTitle(self, num: int) -> str:
+        if not num:
+            return ''
+        q, r = divmod(num-1, 26)
+        return self.convertToTitle(q) + caps[r]
+
+if __name__ == '__main__':
+    S = Solution()
+
+    # assert S.convertToTitle(1) == 'A'
+    # assert S.convertToTitle(25) == 'Y'
+    # assert S.convertToTitle(27) == 'AA'
+    # print(S.convertToTitle(701))
+    # assert S.convertToTitle(702) == 'ZZ'
+
+
+    print(S.convertToTitle(18279))
+
+
