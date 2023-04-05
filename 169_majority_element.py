8c549409b807d20f5a7bca2df42c92f8592af3f1
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-04-05 17:08:58 -0500

    Work Update.

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/majority-element/
 
+from typing import List
+
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
