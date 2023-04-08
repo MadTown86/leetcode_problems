347c3d158341a9266fbf8503bd650023ac6b909a
MadTown86 <grover.donlon@gmail.com>
2023-04-08 14:21:28 -0500

    Finally...

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -7,3 +7,9 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         C = Counter(nums)
         return C.most_common(1)[0][0]
+
+
+if __name__ == '__main__':
+    nums = [2, 2, 2, 1, 1, 3]
+    S = Solution()
+    print(S.majorityElement(nums))
