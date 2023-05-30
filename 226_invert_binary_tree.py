64d9cdb056a993f033b7703402715fe5f64e834f
MadTown86 <grover.donlon@gmail.com>
2023-05-29 21:00:42 -0500

    I need to stay on top of it otherwise I will never understand how to traverse tree structures while simultaneously building them.

--- 226_invert_binary_tree.py
+++ 226_invert_binary_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,16 +7,54 @@ class TreeNode:
         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        newTree = TreeNode(val=root.val)
-        head_newTree = newTree
-        cursor = root
-        left_stack = [root.left]
-        right_stack = [root.right]
+        new_Tree = TreeNode(val=root.val)
+        left_Tree = None
+        right_Tree = None
+        print_stack = []
+        l, r = root.left, root.right
+        print_stack.append(((root.val, (l.val, r.val))))
+        loop_stack = [(l, r)]
+        while loop_stack:
+            l, r = loop_stack.pop()
+            if l and l.left and l.right:
+                l_inp = (l.val, (l.left.val, l.right.val))
+                if l_inp not in print_stack:
+                    print_stack.append(l_inp)
+                    loop_stack.append((l.left, l.right))
+            if r and r.left and r.right:
+                r_inp = (r.val, (r.left.val, r.right.val))
+                if r_inp not in print_stack:
+                    print_stack.append(r_inp)
+                    loop_stack.append((r.left, r.right))
+
+    def invertTree2(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        new_Tree = root
+        n_T = new_Tree
+        traversed = []
+        stack = [root.left, root.right]
+
+        while stack:
+            r, l = stack.pop()
+            new_node = TreeNode(val=r.val, left=r.right, right=r.left)
+            n_T.right = new_node
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+        print(print_stack)
 
 if __name__ == "__main__":
     N15 = TreeNode(15)
@@ -32,4 +72,7 @@ if __name__ == "__main__":
     N3 = TreeNode(3, N6, N7)
     N2 = TreeNode(2, N4, N5)
     NRoot = TreeNode(1, N2, N3)
+
+    S = Solution()
+    S.invertTree(NRoot)
 
