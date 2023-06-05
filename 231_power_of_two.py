284f936b63d008da14fb04a55bdc708b43015423
MadTown86 <grover.donlon@gmail.com>
2023-06-05 15:12:31 -0500

    #231 done.

--- 231_power_of_two.py
+++ 231_power_of_two.py
@@ -2,3 +2,15 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n % 1 != 0:
+            return False
+        if n == 1:
+            return True
+        return self.isPowerOfTwo(n / 2)
+
+if __name__ == '__main__':
+    S = Solution()
+    inp1 = 1
+    print(S.isPowerOfTwo(inp1))
