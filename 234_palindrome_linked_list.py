667277ba74263f2fecd94d6a9ec7e9e5b193873b
MadTown86 <grover.donlon@gmail.com>
2023-06-07 05:39:07 -0500

    #palindrome linked list finished but never satisfied.

--- 234_palindrome_linked_list.py
+++ 234_palindrome_linked_list.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/palindrome-linked-list/
 
-# Definition for singly-linked list.
-# Definition for singly-linked list.
+from collections import Counter
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -9,26 +8,95 @@ class ListNode:
 
 
 class Solution:
+    class ListNode:
+        def __init__(self, val=0, next=None):
+            self.val = val
+            self.next = next
+
     def isPalindrome(self, head: ListNode) -> bool:
-        front_c = 0
-        back_c = 0
-        if head and not head.next:
-            return True
-        if head and head.next and not head.next.next:
-            if head.val == head.next.val:
+
+        #  Container to house linked list values to compare
+        arr = []
+
+        #  Incremental addition to arr until head == None and loop exit
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        #  Conditional cases for len <= 3
+        #  There is an easier way to do this...
+        if len(arr) <= 3:
+            f, s, t = (arr[0], arr[1], arr[2]) if len(arr) == 3 else \
+                (arr[0], arr[1], None) if len(arr) == 2 else (arr[0], None, None) if len(arr) == 1 else \
+                    (None, None, None)
+
+            if f is not None and s is not None and t is not None:
+                if f == t:
+                    return True
+            elif f is not None and s is not None and t is None:
+                if f == s:
+                    return True
+            elif f is not None and s is None and t is None:
                 return True
             return False
-        while head and head.next and head.next.next:
-            print(head.val, head.next.val, head.next.next.val)
-            if head.val <= head.next.val <= head.next.next.val:
-                front_c += 1
-            if head.val >= head.next.val >= head.next.next.val:
-                back_c += 1
-            head = head.next
-            print(front_c, back_c)
-            input()
 
-        return front_c == back_c
+        #  Typing reduction variable - house division of half
+        #  Include center variable for odd sized list
+        half = len(arr) // 2
+
+        #  Create front half slice
+        front = arr[:half]
+
+        #  Create back half slice, add expression to alter for odd size
+        if half * 2 < len(arr):
+            back = arr[half + 1:]
+        else:
+            back = arr[half:]
+
+        #  Reverse back in place
+        back.reverse()
+
+        print(front)
+        print(back)
+
+        #  Return the comparison
+        return front == back
+
+    def isPalindrome2(self, head: ListNode) -> bool:
+
+        # I believe there can be a solution found by counting how many times a certain number is used and ensuring
+        # it fits the palindrome rules
+
+        cnt = Counter()
+        while head:
+            cnt[head.val] += 1
+            head = head.next
+
+        for item, val in cnt.items():
+            print(item, val)
+
+        return None
+
+    def isPalindrome3(self, head: ListNode) -> bool:
+
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        if len(arr) == 3:
+            f, s, t = arr[0], arr[1], arr[2]
+            if f == t and s != t:
+                return True
+            return False
+
+        half = len(arr) // 2
+        front = arr[:half]
+        back = arr[half:]
+        back.reverse()
+        return front == back
+
+
 
 
 if __name__ == '__main__':
@@ -36,8 +104,8 @@ if __name__ == '__main__':
     N6 = ListNode(0)
     N5 = ListNode(2, N6)
     N4 = ListNode(3, N5)
-    N3 = ListNode(3, N4)
-    N2 = ListNode(2, N3)
+    N3 = ListNode(1, )
+    N2 = ListNode(0, N3)
     N1 = ListNode(1, N2)
-    print(S.isPalindrome(N1))
+    print(S.isPalindrome3(N1))
 
