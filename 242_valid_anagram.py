9723d9d3c0c963940bdc70015ebac0df3e6b3bc5
MadTown86 <grover.donlon@gmail.com>
2023-06-13 05:12:14 -0500

    # valid anagram 242 yata

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -1,2 +1,28 @@
+# https://leetcode.com/problems/valid-anagram/submissions/
+
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        dds = defaultdict(int)
+        ddt = defaultdict(int)
+
+        for char in s:
+            dds[char] += 1
+
+        for char in t:
+            ddt[char] += 1
+
+
+        return dds == ddt
+
+if __name__ == '__main__':
+    S = Solution()
+    s = 'anagram'
+    t = 'nagaram'
+
+    a = 'bat'
+    b = 'cat'
+
+    print(S.isAnagram(s, t))
+    print(S.isAnagram(a, b))
