231d0885529f9cb770f08448ac52fd10925b67cd
MadTown86 <grover.donlon@gmail.com>
2023-07-04 07:11:23 -0500

    # 290 finished

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -2,6 +2,25 @@
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        for x, y in zip(pattern, s):
+        key_list = {}
+        if len(pattern) != len(s.split(" ")):
             return False
+        for x, y in zip(pattern, s.split(" ")):
+            if x not in key_list.keys() and y not in key_list.values():
+                key_list[x] = y
+            if x not in key_list.keys() and y in key_list.values():
+                return False
+            if x in key_list.keys():
+                if y != key_list[x]:
+                    return False
+                else:
+                    continue
 
+        return True
+
+
+if __name__ == "__main__":
+    inp = [("abba", "dog fish fish dog"), ("abba", "dog fish fish cat"), ("abba", "dog dog dog dog"), ("aaa", "aa aa aa aa")]
+    S = Solution()
+    for arg in inp:
+        print(S.wordPattern(*arg))
