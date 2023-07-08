f9ac3be53e131b8cd5980ccf64bde5745c578ae7
MadTown86 <grover.donlon@gmail.com>
2023-07-07 22:16:27 -0500

    # Finished 338

--- 190_reverse_bits.py
+++ 190_reverse_bits.py
@@ -1,13 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        x = int(''.join(x for x in reversed(format(n, '032b'))), base=2)
+        x = int(''.join(x for x in reversed(format(n, '0:b'))), base=2)
         return x
 
 if __name__ == "__main__":
     n = int('0b00000010100101000001111010011100', base=2)
     m = int(0b11111111111111111111111111111101)
     S = Solution()
-    S.reverseBits(n)
+    print(S.reverseBits(n))
     S.reverseBits(m)
     assert S.reverseBits(n) == 964176192
     assert S.reverseBits(m) == 3221225471
--- 338_counting_bits.py
+++ 338_counting_bits.py
@@ -2,10 +2,11 @@
 from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        print(str(bin(n), b))
+        return [x for x in [sum([int(y) for y in format(b, '010b')]) for b in range(n+1)]] if n > 0 else [0]
+
 
 if __name__ == "__main__":
     inputs = [1, 2, 3, 4, 5, 6]
     S = Solution()
     for x in inputs:
-        S.countBits(x)
+        print(S.countBits(x))
