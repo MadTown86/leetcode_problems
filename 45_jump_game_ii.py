491bde6ef9a4ec3667f0b883e4d8f53b7b075dbb
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-08-21 11:36:30 -0500

    Work Update.

--- 45_jump_game_ii.py
+++ 45_jump_game_ii.py
@@ -25,8 +25,83 @@ class Solution:
         return f(0)
 
     def jump2(self, nums: List[int]) -> int:
-        i, j, n = 0, 1, len(nums)
-        pass
+        start, n = 0, len(nums)
+        stack = [list(range(start+1, start + nums[start]+1))]
+        count = 0
+        count_stack = []
+        print(stack)
+        while stack:
+            depth_level = stack.pop()
+            print(f'{depth_level=}')
+            count += 1
+            add_container = []
+            for x in depth_level:
+                if isinstance(x, int):
+                    end_range = x + nums[x] + 1
+                    print(f'{x=} :: {depth_level=} :: {end_range=}')
+                    if end_range > n:
+                        count_stack.append(count)
+                        end_range = n-1
+                    add_container.append(list(range(x+1, end_range)))
+                if isinstance(x, list):
+                    for y in x:
+                        end_range2 = y + nums[y] + 1
+                        if end_range2 > n:
+                            count_stack.append(count)
+                            end_range2 = n-1
+                        add_container.append(list(range(y+1, end_range2)))
+            if add_container:
+                stack.append(add_container)
+
+        print(f'{count_stack=}')
+
+    def jump3(self, nums: List[int]) -> int:
+        start, n = 0, len(nums)
+        stack = [list(range(start + 1, start + nums[start] + 1))]
+        count = 0
+        count_stack = []
+        print(stack)
+        while stack:
+            depth_level = stack.pop()
+            print(f'{depth_level=}')
+            count += 1
+            add_container = []
+
+            for x in depth_level:
+                range_builder = []
+                if isinstance(x, int):
+                    step = x + 1
+                    end_range = x + nums[x] + 1
+                    if end_range >= n:
+                        count_stack.append(count)
+                        end_range = n-1
+                    while step < end_range:
+                        range_builder.append(step)
+                        step += 1
+                if isinstance(x, list):
+                    for y in x:
+                        step2 = y + 1
+                        end_range = y + nums[y] + 1
+                        if end_range >= n:
+                            count_stack.append(count)
+                            end_range = n-1
+                        while step2 < end_range:
+                            range_builder.append(step2)
+                            step2 += 1
+                add_container.append(range_builder)
+
+
+
+            if add_container:
+                stack.append(add_container)
+
+        print(f'{count_stack=}')
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -34,4 +109,4 @@ if __name__ == "__main__":
     inputs = [[2, 3, 1, 1, 4], [2, 3, 0, 1, 4], [4, 1, 1, 1, 1]]
 
     for x in inputs:
-        S.jump(x)
+        S.jump3(x)
