fd235632e7d7a2b1350cf12bd83037bd0bcc982c
MadTown86 <grover.donlon@gmail.com>
2023-08-26 21:02:40 -0500

    badabing

---
+++ 1306_jump_game_III.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/jump-game-iii/description/
+
+from typing import List
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+
+        def f(x):
+            if x == 0
+            positive = x + arr[x]
+            negative = x - arr[x]
+            if positive <
+
+        pass
