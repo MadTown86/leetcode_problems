cb0aab24507865f7c924e6e8572fff1b534d8517
MadTown86 <grover.donlon@gmail.com>
2023-09-05 10:33:33 -0500

    Need to rethink #402.

--- 402_remove_k_digits.py
+++ 402_remove_k_digits.py
@@ -5,29 +5,29 @@ import math
 
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        if k == len(num):
-            return "0"
-        res, front = num[k:], num[:k]
-        b = 0
-        while front:
-            digit = front[0]
-            if digit <= min(front) and digit < res[b]:
-                res = res[:b] + digit + res[b + 1 :]
-                front = front[1:]
-                b += 1
-            elif digit > min(front):
-                front = front[1:]
-            elif digit <= min(front) and digit > res[b]:
-                front = front[1:]
-                b += 1
-            elif digit <= min(front) and digit < res[b]:
-                front = front[1:]
+        res = num[k:]
+        a, b = 0, k
+        print(num[a], num[k])
+        while k:
+            if num[a] > num[b]:
+                k -= 1
+                a += 1
 
-        return res.lstrip("0")
+
+
+
+
+
 
 
 if __name__ == "__main__":
     S = Solution()
+    with open("C:\\REPOSITORIES\\PYTHON\\leetcode_problems\\402_input.txt", 'r') as inp:
+        txt = inp.read()
+
+    with open("C:\\REPOSITORIES\\PYTHON\\leetcode_problems\\402_answer.txt", 'r') as ansfile:
+        ans = ansfile.read()
+
     inputs = [
         ("1432219", 3),
         ("10200", 1),
@@ -39,6 +39,11 @@ if __name__ == "__main__":
         ("12345", 4),
         ("54321", 3),
         ("12345", 3),
+        ("10", 1),
+        ("100", 1),
+        ("112", 1),
+        ("12345", 2),
+        (txt, 1000)
     ]
 
     assert S.removeKdigits(*inputs[0]) == "1219"
@@ -49,3 +54,14 @@ if __name__ == "__main__":
     assert S.removeKdigits(*inputs[5]) == "0"
     assert S.removeKdigits(*inputs[6]) == "1"
     assert S.removeKdigits(*inputs[7]) == "1"
+    assert S.removeKdigits(*inputs[8]) == "21"
+    assert S.removeKdigits(*inputs[9]) == "12"
+    assert S.removeKdigits(*inputs[10]) == "0"
+    assert S.removeKdigits(*inputs[11]) == "0"
+    assert S.removeKdigits(*inputs[12]) == "11"
+    assert S.removeKdigits(*inputs[13]) == "123"
+    # myres = S.removeKdigits(*inputs[14])
+    # for mine, theirs in zip(myres, ans):
+    #     print(f'{mine=} :: {theirs=}')
+    # assert S.removeKdigits(*inputs[14]) == ans
+
