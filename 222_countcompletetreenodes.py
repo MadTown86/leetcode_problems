8ec69f94d058ae600a971d492bfd0a13b29d939d
MadTown86 <grover.donlon@gmail.com>
2023-09-06 12:26:45 -0500

    222 Finished but not logarithmic time.

--- 222_countcompletetreenodes.py
+++ 222_countcompletetreenodes.py
@@ -7,10 +7,39 @@ class TreeNode:
         self.right = right
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
-        left = root.left
-        right = root.right
-
+        # print(root.val)
+        if not root:
+            return 0
         counter = 0
+        if root.left and root.right:
+            left = root.left
+            right = root.right
+
         while left.left and right.right:
-            left.left = left.left.left
-            right.right = right.right.right
+            last_left = left
+            last_right = right
+            left = left.left
+            right = right.right
+            counter += 1
+
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
+
+
+
+
+if __name__ == "__main__":
+    S = Solution()
+    N12 = TreeNode(12)
+    N11 = TreeNode(11)
+    N10 = TreeNode(10)
+    N9 = TreeNode(9)
+    N8 = TreeNode(8)
+    N7 = TreeNode(7)
+    N6 = TreeNode(6, N12)
+    N5 = TreeNode(5, N10, N11)
+    N4 = TreeNode(4, N8, N9)
+    N3 = TreeNode(3, N6, N7)
+    N2 = TreeNode(2, N4, N5)
+    N1 = TreeNode(1, N2, N3)
+
+    print(S.countNodes(N1))
