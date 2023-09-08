1f7bf7129738f973a8b76df93dbe1874375b5a52
MadTown86 <grover.donlon@gmail.com>
2023-09-08 07:53:14 -0500

    Speed was there, memory usage was high.

--- 392_issubsequence.py
+++ 392_issubsequence.py
@@ -2,15 +2,34 @@
 import re
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pattern, st = 0, 0
-
-        while pattern < len(s):
-            if s[pattern] in t:
+        pattern, st, lt, ls = 0, 0, len(t), len(s)
+        if ls == 0:
+            return True
+        if ls > lt:
+            return False
+        while pattern < ls:
+            if ls-1-pattern > lt-st:
+                return False
+            if st < lt:
+                if pattern == ls - 1 and st >= pattern and t[st] == s[pattern]:
+                    return True
+                elif s[pattern] == t[st] and st >= pattern:
+                    pattern += 1
+                    st += 1
+                else:
+                    st += 1
+            else:
+                return False
+        return False
 
 
 if __name__ == "__main__":
-    inp = [('abc', 'ahbgdc'), ('axc', 'ahbgdc')]
+    inp = [('abc', 'ahbgdc'), ('axc', 'ahbgdc'), ('a', 'd'), ('abcd', 'adddbaaaeeecfffffeeed'), ('', ''), ('abc', 'a')]
     S = Solution()
     S.isSubsequence(*inp[0])
-    # assert S.isSubsequence(*inp[0]) == True
-    # assert S.isSubsequence(*inp[1]) == False
+    assert S.isSubsequence(*inp[0]) == True
+    assert S.isSubsequence(*inp[1]) == False
+    assert S.isSubsequence(*inp[2]) == False
+    assert S.isSubsequence(*inp[3]) == True
+    assert S.isSubsequence(*inp[4]) == True
+    assert S.isSubsequence(*inp[5]) == False
