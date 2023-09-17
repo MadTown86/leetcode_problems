d2630e123137b318bc4e3340bc0ae086af3c6d5c
MadTown86 <grover.donlon@gmail.com>
2023-09-17 05:19:54 -0500

    Virtual Weekly Text #1

---
+++ 2859_sum_of_values_at_indices_kbits.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/contest/weekly-contest-363/problems/sum-of-values-at-indices-with-k-set-bits/
+
+from typing import List
+
+
+class Solution:
+    """
+    Should have just stayed up for the weekly test.  At least would have gotten one answer.
+    """
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        s = 0
+        for x in range(len(nums)):
+            if sum(int(x) for x in "{0:b}".format(x)) == k:
+                s += nums[x]
+        return s
+
+
+if __name__ == "__main__":
+    S = Solution()
+    inp = [([5, 10, 1, 5, 2], 1), ([4, 3, 2, 1], 2)]
+
+    assert S.sumIndicesWithKSetBits(*inp[0]) == 13
+    assert S.sumIndicesWithKSetBits(*inp[1]) == 1
