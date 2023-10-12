1c185f34c25e1d56307aaf6fb855c3f172874c81
MadTown86 <grover.donlon@gmail.com>
2023-10-12 06:43:00 -0500

    441 finish

--- 441_arrangingcoins.py
+++ 441_arrangingcoins.py
@@ -2,7 +2,44 @@
 import math
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        calc1 = bool(math.sqrt(5 * (n**2) + 4) % 1 == 0)
-        calc2 = bool(math.sqrt(5*(n**2)-4) % 1 == 0)
+        flights = 0
+        if n < 4:
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
+            if n == 3:
+                return 2
+        cursor = 0
+        counter = 1
+        while cursor <= n:
+            cursor += counter
+            counter += 1
+            if cursor <= n:
+                flights += 1
+            print(f'{cursor=}, {counter=}, {flights=}')
 
-        if calc1 and calc2:
+        return flights
+
+# Better Solution
+"""
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left,right=1,n
+        while left<=right:
+            mid=(right+left)//2
+            num=(mid/2)*(mid+1)
+            if num<=n:
+                left=mid+1
+            else:
+                right=mid-1
+        return right
+"""
+
+
+
+
+if __name__ == "__main__":
+    S = Solution()
+    inp = 20
+    print(S.arrangeCoins(inp))
