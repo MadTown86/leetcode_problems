a23706876e7483bf0bc42bf1464f80f379a180a7
MadTown86 <grover.donlon@gmail.com>
2024-10-01 14:56:58 -0500

    Completed another, however, exponential time complexity.  Better to check strings to see if they are divisible by the length of the substring and then multiply substring by this amount to check against string instead of 'build as you go' like I did initially.

--- 459_repeated_substring_pattern.py
+++ 459_repeated_substring_pattern.py
@@ -1,9 +1,38 @@
 import re
 
 def main(s: str)-> bool:
-    pass
+        x = 1
+        while x < len(s):
+            sub = s[:x]
+            print(f'{sub=}')
+            mult = 1
+            trialstring = ""
+            while len(trialstring) <= len(s):
+                trialstring = sub * mult
+                if trialstring == s:
+                    return True
+                else:
+                    mult += 1
+            x += 1
+        return False
+
+"""faster solution O(n)
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, n//2 + 1):
+        # This excludes uneven strings that can't be divided into equal parts, smart
+            if n % i == 0:
+                substring = s[:i]
+                # Based on the 'even' division, you only need to check to see if the substring multiplied by cursor is equal to S also smart
+                    # don't have to build entire string and then check
+                if substring * (n // i) == s:
+                    return True
+        return False
+"""
+
 
 if __name__ == "__main__":
-    inputs = []
+    inputs = ["abab", "aba", "abcabcabcabc"]
     for i in inputs:
         print(main(i))
